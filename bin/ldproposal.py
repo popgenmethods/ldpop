a234b6c4e63f6120308623eb78b141df3707120a
Jeff <spence.jeffrey@berkeley.edu>
2016-03-08 10:40:58 -0500

    updated ldproposal args
    updated ldproposal args to match ldtable.
    
    will update java args to match, and commit java once all of this is
    tested.

--- bin/ldproposal.py
+++ bin/ldproposal.py
@@ -7,15 +7,15 @@ import logging, argparse
 ## make the command line args similar to ldtable.py
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--numHaps", type=int)
-    parser.add_argument("--theta", type=float)
-    parser.add_argument("--popSizes", type=str)
-    parser.add_argument("--epochTimes", type=str)
-    parser.add_argument("--cores", type=int, default=1)
-    parser.add_argument("--ldHelmetRhos", type=str)
-    parser.add_argument("--timePointsPerEpoch", type = int)
-    parser.add_argument("--log", type=str)    
+    parser = argparse.ArgumentParser(description="Print a table of (approximate) likelihoods through time, used for importance sampling")
+    parser.add_argument("-n", type = int, metavar = "N", required = True, help = "sample size (table will contain configs with 2N haplotypes)" )
+    parser.add_argument("-th", type=float, metavar = "theta", required = True, help = "twice the mutation rate")
+    parser.add_argument("-rh", type=str, metavar = "rho", required = True, help = "twice the recomb rate.  Alternatively, one can input num_rho,max_rho to make a grid with num_rh uniformly spaced points from 0 to max_rh, inclusive. (((Alternatively, to create a non-uniform grid, use '-rh r0,step0,r1,step1,r2,...rK'. This creates a grid {r0,r0+step0,r0+2*step0,...,r1,r1+step1,...,rK} similar to ldhelmet. Note that non-uniform grid is incompatible with vanilla ldhat.)))")
+    parser.add_argument("-s", type=str, metavar="s0,s1,...,sD", help="coalescent scaled population sizes (s0=present size, sD=ancient size)")
+    parser.add_argument("-t", type=str, metavar="t1,...,tD", help="times of size changes from present backwards. Must be increasing positive reals.")
+    parser.add_argument("--cores", type=int, default=1, help="Number of parallel processes to use.")
+    parser.add_argument("--grdpts", default = 5, metavar = "D", type = int, help = "Number of points per epoch to discretize time.  More points will results in a longer run time, but more efficient importance sampling.")
+    parser.add_argument("--log", type=str, metavar="logfile", help="Log extra info to logfile. If logfile='.', logs to STDERR.")     
     args = parser.parse_args()
 
     if args.log == ".":
@@ -23,22 +23,17 @@ if __name__ == '__main__':
     elif args.log is not None:
         logging.basicConfig(filename=args.log, level=logging.INFO)
     
-    rhos = rhos_from_string(args.ldHelmetRhos)
+    rhos = rhos_from_string(args.rh)
     
-    theta = args.theta
     
-    assert (args.popSizes is None) == (args.epochTimes is None)
-    if args.popSizes is None:
-        popSizes = [1]
+    assert (args.s is None) == (args.t is None)
+    if args.s is None:
+        popSizes = [1.]
         times = []
     else:
-        popSizes = [float(i) for i in args.popSizes.split(",")]
-        times = map(float, args.epochTimes.split(","))
+        popSizes = [float(i) for i in args.s.split(",")]
+        times = map(float, args.t.split(","))
 
-    assert len(popSizes) == len(times)+1
-    times = [float(i) for i in args.epochTimes.split(",")]
+    assert len(popSizes) == len(times)+1    
     
-    numTimePointsPerEpoch = args.timePointsPerEpoch
-    
-    
-    print ISProposal(args.numHaps, theta, rhos, popSizes, times, numTimePointsPerEpoch, args.cores)
+    print ISProposal(args.n, args.th, rhos, popSizes, times, args.grdpts, args.cores)
