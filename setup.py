8eb9ae327e1c687cd145c6089dcb9cc8ce9d432d
Jack Kamm <jackkamm@gmail.com>
2018-03-16 08:16:48 +0000

    allow setup.py to work without prior numpy installation

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
-import numpy
-
 
 setup(name='ldpop',
       version='0.1.0',
       description='Computing the two-locus sampling probability for variable population size',
       author='Jack Kamm, Jeffrey Spence, Jeffrey Chan, Yun S. Song',
       author_email='jkamm@stat.berkeley.edu, spence.jeffrey@berkeley.edu, chanjed@cs.berkeley.edu, yss@eecs.berkeley.edu',
-      packages=['ldpop'],      
+      packages=['ldpop'],
       install_requires=['numpy','scipy','pandas','future'],
       keywords=['population genetics','statistics','moran model','coalescent'],
       )
